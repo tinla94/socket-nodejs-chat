9040ed09066167380584e4a0e162c606d4d45aeb
Tin La <tinla14@gmail.com>
2019-09-09 15:07:55 -0700

    Setup file

---
+++ src/index.js
@@ -0,0 +1,10 @@
+const express = require('express');
+const path = require('path');
+const port = process.env.PORT || 3000;
+const publicDirectoryPath = path.join(__dirname, '../public');
+const app = express();
+
+
+app.use(express.static(publicDirectoryPath));
+
+app.listen(port, () => console.log(`Listening to port ${port}`));

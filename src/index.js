f2229a69faf819ebf07fbb28456579f2ff78f4b7
Tin La <tinla14@gmail.com>
2019-09-09 15:27:26 -0700

    Finished setup socket.io for nodejs

--- src/index.js
+++ src/index.js
@@ -1,10 +1,21 @@
+const http = require('http');
 const express = require('express');
+const socketio = require('socket.io');
 const path = require('path');
-const port = process.env.PORT || 3000;
 const publicDirectoryPath = path.join(__dirname, '../public');
+// setting up server 
 const app = express();
+const server = http.createServer(app);
+const io = socketio(server)
 
 
 app.use(express.static(publicDirectoryPath));
 
-app.listen(port, () => console.log(`Listening to port ${port}`));
+// connecting socketio
+io.on('connection', () => {
+    console.log('New webscoket connection!')
+})
+// port
+const port = process.env.PORT || 3000;
+server.listen(port, () => console.log(`Listening to port ${port}`));
+

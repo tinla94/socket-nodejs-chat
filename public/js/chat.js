0c6a1c43c94f6bcfc2bc2efaad6c360551eeb6d6
Tin La <tinla14@gmail.com>
2019-09-10 14:52:54 -0700

    Added actions acknowledge

--- public/js/chat.js
+++ public/js/chat.js
@@ -1,28 +1,31 @@
 const socket = io();
-socket.on('message', (message) => {
-    console.log(message);
+socket.on("message", message => {
+  console.log(message);
 });
 
+document.querySelector("#message-form").addEventListener("submit", e => {
+  e.preventDefault();
 
+  const message = e.target.elements.message.value;
 
-document.querySelector("#message-form").addEventListener('submit', (e) => {
-    e.preventDefault();
-
-    const message = e.target.elements.message.value
-
-    socket.emit('sendMessage', message)
+  socket.emit("sendMessage", message, (error) => {
+      if(error) console.log(error);
+      console.log('Message is sent!')
+  });
 });
 
-document.querySelector('#send-location').addEventListener('click', (e) => {
-    e.preventDefault();
-    if(!navigator.geolocation) {
-        return alert('This location is not supported by your browser!');
-    }
+document.querySelector("#send-location").addEventListener("click", e => {
+  e.preventDefault();
+  if (!navigator.geolocation) {
+    return alert("This location is not supported by your browser!");
+  }
 
-    navigator.geolocation.getCurrentPosition((position) => {
-        socket.emit('sendLocation', {
-            latitude: position.coords.latitude,
-            longtitude: position.coords.longitude
-        });
+  navigator.geolocation.getCurrentPosition(position => {
+    socket.emit("sendLocation", {
+      latitude: position.coords.latitude,
+      longtitude: position.coords.longitude
+    }, () => {
+        console.log('Location shared!')
     });
+  });
 });
--- src/index.js
+++ src/index.js
@@ -3,6 +3,7 @@ const express = require('express');
 const socketio = require('socket.io');
 const path = require('path');
 const publicDirectoryPath = path.join(__dirname, '../public');
+const Filter = require('bad-words');
 // setting up server 
 const app = express();
 const server = http.createServer(app);
@@ -18,12 +19,20 @@ io.on('connection', (socket) => {
     // setup  counting for accessing
     socket.emit('message', 'Welcome!');
     socket.broadcast.emit('message', 'A new user has joined chat!');
-    socket.on('sendMessage', (message) => {
+
+    socket.on('sendMessage', (message, callback) => {
+        const filter = new Filter();
+
+        if(filter.isProfane(message)) {
+            return callback('Profanity is not allowed!');
+        }
         io.emit('message', message);
+        callback();
     });
 
-    socket.on('sendLocation', (coords) => {
-        io.emit('message', `https://google.com/maps?q=${coords.latitude},${coords.longtitude}`)
+    socket.on('sendLocation', (coords, callback) => {
+        io.emit('message', `https://google.com/maps?q=${coords.latitude},${coords.longtitude}`);
+        callback()
     });
 
     // disconnecting

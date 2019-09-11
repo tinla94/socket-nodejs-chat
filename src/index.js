d6cd91abe40f6880b4b3fa0dfe2cd17b71554ccd
Tin La <tinla14@gmail.com>
2019-09-11 10:17:10 -0700

    Added username

--- src/index.js
+++ src/index.js
@@ -37,26 +37,29 @@ io.on('connection', (socket) => {
             return callback(error);
         }
         socket.join(user.room); // joining room 
-        socket.emit('message', generateMessage(`Welcome to room ${user.room}`));
-        socket.broadcast.to(user.room).emit('message', generateMessage(`${user.username} has joined the room!`));
+        socket.emit('message', generateMessage('Admin', `Welcome to room ${user.room}`));
+        socket.broadcast.to(user.room).emit('message', generateMessage('Admin', `${user.username} has joined the room!`));
 
-        callback()
+        callback();
     });
 
     // sending message
     socket.on('sendMessage', (message, callback) => {
+        const user = getUser(socket.id)
         const filter = new Filter();
 
         if(filter.isProfane(message)) {
             return callback('Profanity is not allowed!');
         }
-        io.to('Center City').emit('message', generateMessage(message))
+        io.to(user.room).emit('message', generateMessage(user.username, message))
         callback();
     });
 
-    socket.on('sendLocation', (coords, callback) => {
-        io.emit('locationMessage', generateLocation(`https://google.com/maps?q=${coords.latitude},${coords.longtitude}`));
-        callback()
+    socket.on('sendLocation', (coords, callback) => {        
+        const user = getUser(socket.id)
+        // send location
+        io.to(user.room).emit('locationMessage', generateLocation(user.username, `https://google.com/maps?q=${coords.latitude},${coords.longtitude}`));
+        callback();
     });
 
     // disconnecting
@@ -64,7 +67,7 @@ io.on('connection', (socket) => {
         const user = removeUser(socket.id);
 
         if (user) {
-            io.to(user.room).emit('message', generateMessage(`${user} has left the room!`));
+            io.to(user.room).emit('message', generateMessage('Admin', `${user} has left the room!`));
         }
     });
 });
